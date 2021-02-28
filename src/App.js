8294f1beec68de25c0ce185e7b1e0f504275aaf9
abhinav <ambhinav@yahoo.com>
2021-02-28 23:52:02 +0800

    set up basic ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Button, Container, Grid, TextField, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { lightGreen } from '@material-ui/core/colors';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    margin: theme.spacing(1),
+  },
+  search: {
+    '& > *': {
+      width: '55ch',
+    },
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <Grid contianer>
+        <Grid item container justify='center'>
+          <Typography variant="h3" style={{ padding: '10px' }}>Compare Star Wars Characters!</Typography>
+        </Grid>
+      </Grid>
+      <Grid container justify='center' alignItems='center' spacing={4}>
+        <Grid className={classes.search} item>
+          <TextField label="Enter Star Wars Character" variant="outlined"></TextField>
+        </Grid>
+        <Grid item>
+          <Button variant='outlined' variant='contained' color="primary" size="large">Go!</Button>
+        </Grid>
+      </Grid>
     </div>
   );
 }
